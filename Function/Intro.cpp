74e3e9889199e6810218ce587895f36280a4ee97
adeeljameel810-byte <adeeljameel810@gmail.com>
2025-11-06 22:52:29 +0500

    Add introduction function in Intro.cpp

---
+++ Function/Intro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void introduce() {
+  cout <<"Hello Everyone !" << endl;
+  cout <<"My name is Jameel Ahmed ." <<endl;
+cout << "I am learning C++ programming ." <<endl;
+cout << "I love to use GitHub and share my code !" <<endl;
+}
+int main() { 
+   introduce();   //calling the function 
+return 0;
+}
