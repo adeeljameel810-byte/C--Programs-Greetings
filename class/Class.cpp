d669d0e0ff437116b4d2a32ddf3302520a5516eb
adeeljameel810-byte <adeeljameel810@gmail.com>
2025-11-11 12:43:59 +0500

    Add Car class with displayInfo method

--- class/Class.cpp
+++ class/Class.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+class Car {
+public:
+    string brand;
+    int speed;
+
+    void displayInfo() {
+        cout << "Brand: " << brand << ", Speed: " << speed << " km/h" << endl;
+    }
+};
+
+int main() {
+    Car car1;
+    car1.brand = "BMW";
+    car1.speed = 180;
+
+    car1.displayInfo();
+     return 0;
+}
